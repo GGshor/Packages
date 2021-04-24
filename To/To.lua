35372dc6092a7e717842230f77a9f82773d388f4
GGshor <scorpius514@gmail.com>
2021-04-24 10:59:43 +0200

    Added the code

--- To/To.lua
+++ To/To.lua
@@ -1,1 +1,28 @@
-print("Hi")
+local module = {
+	Name = "To",
+	Description = "Teleport to a player",
+	Location = "Player",
+}
+
+module.Execute = function(Client, Type, Attachment)
+	if Type == "command" then
+		local char = module.API.getCharacter(module.API.getPlayerWithName(Attachment))
+		if char then
+            if Client.Character:FindFirstChild("HumanoidRootPart") and char:FindFirstChild("HumanoidRootPart") then
+                if Client.Character.Humanoid.Sit then
+                    Client.Character.Humanoid.Sit = false
+                    Client.Character.Humanoid.Jump = true
+                end
+                wait()
+			    Client.Character.HumanoidRootPart.CFrame = (char.HumanoidRootPart.CFrame*CFrame.Angles(0,math.rad(90/1),0)*CFrame.new(5+.2*1,0,0))*CFrame.Angles(0,math.rad(90),0)
+                return true
+            else
+			    return false
+            end
+		end
+        return false
+	end
+end
+
+
+return module
